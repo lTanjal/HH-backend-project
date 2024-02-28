426f8f8becf5dc13231ad1deff7351188537346a
Tatiana <tatiana.lyubavskaya@gmail.com>
2024-02-28 14:16:19 +0200

    REST added

--- src/main/java/backendproject/Bookstore/domain/Category.java
+++ src/main/java/backendproject/Bookstore/domain/Category.java
@@ -2,6 +2,8 @@ package backendproject.Bookstore.domain;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,6 +18,7 @@ public class Category {
     private Long categoryId;
     private String categoryName;
 
+    @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
 	private List<Book> books;
 
--- src/main/java/backendproject/Bookstore/web/BookController.java
+++ src/main/java/backendproject/Bookstore/web/BookController.java
@@ -1,5 +1,6 @@
 package backendproject.Bookstore.web;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import backendproject.Bookstore.domain.Book;
 import backendproject.Bookstore.domain.BookRepository;
@@ -47,13 +49,25 @@ public class BookController {
 		return "redirect:/booklist";
 
 	}
-	 
+
 	@RequestMapping(value = "/editbook/{id}", method = RequestMethod.GET)
 	public String editBooks(@PathVariable("id") Long bookId, Model model) {
-	Optional<Book> book= repository.findById(bookId);
-	model.addAttribute("book", book );
-	model.addAttribute("categories", crepository.findAll());
-	return "editbook";
+		Optional<Book> book = repository.findById(bookId);
+		model.addAttribute("book", book);
+		model.addAttribute("categories", crepository.findAll());
+		return "editbook";
 	}
-	
+
+	// REST
+	@RequestMapping(value = "/booklistjson", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+
+	// REST
+	@RequestMapping(value = "/bookjson/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+		return repository.findById(bookId);
+
+	}
 }
